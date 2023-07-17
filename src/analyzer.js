fb5b39399fa2dddff318dae04afd265db6d6972f
kimmvb <kimveb1@gmail.com>
2023-07-17 10:37:17 -0500

    Contador de palabras, caracteres y caracteres sin espacio.

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,9 +1,26 @@
+
 const analyzer = {  
+
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    function wordCount() {  
+      const totaltext = document.querySelector('textarea[name="user-input"]').value;
+      let count1 = 0;
+      const splittext = totaltext.split(' ');
+      for (let i = 0; i < splittext.length; i++) {
+        if (splittext[i] !== "") {
+          count1++;
+        }
+      }
+      document.querySelector(".word-count").innerHTML = "Palabras: " + count1;
+      document.querySelector(".character-count").innerHTML = "Caracteres: " + totaltext.length;
+      document.querySelector(".character-no-spaces-count").innerHTML = "Caracteres sin espacios: " + totaltext.trim().length;
+    }
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+document.getElementById("reset-button").addEventListener("click", reload)
+
+function reload() {
+  document.location.reload()
+}
+
+document.querySelector('textarea[name="user-input"]').addEventListener("keyup", wordCount)
+
+function wordCount() {
+  const totaltext = document.querySelector('textarea[name="user-input"]').value;
+  let count1 = 0;
+  const splittext = totaltext.split(' ');
+  for (let i = 0; i < splittext.length; i++) {
+    if (splittext[i] !== "") {
+      count1++;
+    }
+  }
+  document.querySelector(".word-count").innerHTML = "Palabras: " + count1;
+  document.querySelector(".character-count").innerHTML = "Caracteres: " + totaltext.length;
+  document.querySelector(".character-no-spaces-count").innerHTML = "Caracteres sin espacios: " + totaltext.trim().length;
+}
