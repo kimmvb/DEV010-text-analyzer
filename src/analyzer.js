450b41bc0e12abd0bdc9e81a857e527061139773
kimmvb <kimveb1@gmail.com>
2023-07-21 09:46:01 -0500

    Contador de números, suma de números y longitud de palabras.

--- src/analyzer.js
+++ src/analyzer.js
@@ -5,39 +5,63 @@ const analyzer = {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`
     
     //text = document.querySelector('textarea[name="user-input"]').value;  
-    let count1 = 0;
+    let count = 0;
     const splittext = text.split(' ');
     for (let i = 0; i < splittext.length; i++) {
       if (splittext[i] !== "") {
-        count1++;
+        count++;
       }
     }
-    return count1; 
+    return count; 
   },
 
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-     
-    const splittext = text.length
-    
-    return splittext
+    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.  
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    const splittext = text.trim().length
+    const regex = text.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_'{|}~\s]/g, "");
     
-    return splittext
-
+    return regex.length;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let count = 0;
+    const splittext = text.split(' ').length;
+    const textArray = text.split(' ');
+    for (let i = 0; i < splittext; i++) { 
+      count += textArray[i].length; 
+    }
+    const wordAverage = count / splittext;
+    const fixedAverage = wordAverage.toFixed(2);
+    return parseFloat(fixedAverage)
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let count = 0
+    const textSplit = text.split(' ')
+    for ( let i = 0; i < textSplit.length; i++) {
+      if (Number(textSplit[i])) {
+        count = count + 1
+      }
+    }
+    return count
+
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
+    let count = 0;
+    const textSplit = text.trim().split(' ');
+    for ( let i = 0; i < textSplit.length; i++) {
+      const element = textSplit[i]
+      if (!isNaN(element) && element !== "") {
+        count = count + (element*1);
+      }
+    }
+    return count 
+  }
 };
+
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 //variable que es igual a "llamar" al doc. HTML contador de palabras
-const wordCount = document.querySelector(".word-count")
-const characterCount = document.querySelector(".character-count")
-const characterCountNoSpaces = document.querySelector(".character-no-spaces-count")
+const wordCount = document.querySelector('li[data-testid="word-count"]');
+const characterCount = document.querySelector('li[data-testid="character-count"]');
+const characterCountNoSpaces = document.querySelector('li[data-testid="character-no-spaces-count"]');
+const numberCount = document.querySelector('li[data-testid="number-count"]');
+const numberSum = document.querySelector('li[data-testid="number-sum"]');
+const averageWordLength = document.querySelector('li[data-testid="word-length-average"]');
 
-const textarea = document.querySelector('textarea[name="user-input"]')//variable que es igual a "llamar" al doc. HTML el área de texto
+const textarea = document.querySelector('textarea[name="user-input"]');//variable que es igual a "llamar" al doc. HTML el área de texto
 
 textarea.addEventListener("keyup", function (event) { 
   const quantityWords = analyzer.getWordCount(event.target.value); 
@@ -17,7 +20,13 @@ textarea.addEventListener("keyup", function (event) {
   characterCount.innerHTML = "Caracteres: " + quantityCharacters;
   const quantityCharactersNoSpaces = analyzer.getCharacterCountExcludingSpaces(event.target.value);
   characterCountNoSpaces.innerHTML = "Caracteres sin espacios: " + quantityCharactersNoSpaces
-} )
+  const quantityNumbers = analyzer.getNumberCount(event.target.value);
+  numberCount.innerHTML = "Números: " + quantityNumbers;
+  const quantityNumbersSum = analyzer.getNumberSum(event.target.value);
+  numberSum.innerHTML = "Suma de números: " + quantityNumbersSum;
+  const totalAverageWordLength = analyzer.getAverageWordLength(event.target.value);
+  averageWordLength.innerHTML = "Promedio de longitud: " + totalAverageWordLength;
+} );
 //conecto la variable anterior del área de texto con un evento que sucede por apretar alguna tecla (keyup) con una función con parámetro evento que sigue las siguientes instrucciones: 1. crea una variable que contiene el número total de palabras, gracias a llamar a el proceso del analizer getWordCount(objeto,método), teniendo como propiedad traer el valor(value) del text.area   2. se llama all contenido de la varible wordCount para cambiarlo por la cantidad de palabras
 
 
